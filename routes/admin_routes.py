12061be3c1a3bcdccf306a636200e0acf9e37c94
Mirielle Rosa <mirielle.r.souza@gmail.com>
2024-09-18 13:28:08 -0300

    atualização do dia 11/09

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Path, Request, Response
+from fastapi import APIRouter, Path
 from fastapi.responses import JSONResponse
 
 from dtos.alterar_pedido_dto import AlterarPedidoDto
@@ -12,7 +12,7 @@ from repositories.pedido_repo import PedidoRepo
 from repositories.produto_repo import ProdutoRepo
 
 
-router = APIRouter(prefix="/manager")
+router = APIRouter(prefix="/admin")
 
 
 @router.get("/obter_produtos")
